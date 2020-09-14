e53f794ec84886c00b292aa715080682bfa94f07
Kyriakos Barbounakis <k.barbounakis@gmail.com>
2020-09-14 10:28:11 +0300

    correct DataObject typescript definition

--- data-model.js
+++ data-model.js
@@ -54,14 +54,8 @@ function inferTagMapping(field) {
     if (_.isNil(field)) {
         return;
     }
-    var hasManyAttribute = Object.prototype.hasOwnProperty.call(field, 'many');
-    // if field does not have attribute 'many'
-    if (hasManyAttribute === false) {
-        // do nothing
-        return;
-    }
-    // if field has attribute 'many' but it's false
-    if (hasManyAttribute === true && field.many === false) {
+    //validate DataField.many attribute
+    if (Object.prototype.hasOwnProperty.call(field, 'many') && field.many === false) {
         return;
     }
     //check if the type of the given field is a primitive data type
