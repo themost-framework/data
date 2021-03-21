e3ac36e2fc80f50b674b7f926df54298786e7444
Kyriakos Barbounakis <k.barbounakis@gmai.com>
2021-03-21 19:10:50 +0200

    update listeners

--- module-loader.js
+++ module-loader.js
@@ -1,6 +1,6 @@
 // MOST Web Framework 2.0 Codename Blueshift BSD-3-Clause license Copyright (c) 2017-2021, THEMOST LP All rights reserved
-var path = require('path');
-var executionPathProperty = Symbol('executionPath');
+const path = require('path');
+const executionPathProperty = Symbol('executionPath');
 /**
  * @class
  * @abstract
--- previous-state-listener.js
+++ previous-state-listener.js
@@ -1,17 +1,9 @@
-/**
- * @license
- * MOST Web Framework 2.0 Codename Blueshift
- * Copyright (c) 2017, THEMOST LP All rights reserved
- *
- * Use of this source code is governed by an BSD-3-Clause license that can be
- * found in the LICENSE file at https://themost.io/license
- */
-///
+// MOST Web Framework 2.0 Codename Blueshift BSD-3-Clause license Copyright (c) 2017-2021, THEMOST LP All rights reserved
+
 function previousStateListener(event, callback) {
-    var _ = require("lodash");
     if (event.state===1) { return callback(); }
     var key = event.model.primaryKey;
-    if (_.isNil(event.target[key])) {
+    if (event.target[key] == null) {
         return callback();
     }
     event.model.where(key).equal(event.target[key]).silent().first(function(err,result) {
@@ -25,20 +17,21 @@ function previousStateListener(event, callback) {
     });
 }
 
-/**
- * @module @themost/data/previous-state-listener
- * @ignore
- */
-
-/**
- * Occurs before creating or updating a data object.
- * @param {DataEventArgs|*} event - An object that represents the event arguments passed to this operation.
- * @param {Function} callback - A callback function that should be called at the end of this operation. The first argument may be an error if any occurred.
- */
-exports.beforeSave = previousStateListener;
-/**
- * Occurs before removing a data objects.
- * @param {DataEventArgs|*} event - An object that represents the event arguments passed to this operation.
- * @param {Function} callback - A callback function that should be called at the end of this operation. The first argument may be an error if any occurred.
- */
-exports.beforeRemove = previousStateListener;
+module.exports = {
+    /**
+     * Occurs before creating or updating a data object.
+     * @param {DataEventArgs|*} event - An object that represents the event arguments passed to this operation.
+     * @param {Function} callback - A callback function that should be called at the end of this operation. The first argument may be an error if any occurred.
+     */
+    beforeSave(event, callback) {
+        return previousStateListener(event, callback);
+    },
+    /**
+     * Occurs before removing a data objects.
+     * @param {DataEventArgs|*} event - An object that represents the event arguments passed to this operation.
+     * @param {Function} callback - A callback function that should be called at the end of this operation. The first argument may be an error if any occurred.
+     */
+    beforeRemove(event, callback) {
+        return previousStateListener(event, callback);
+    }
+}
