64a0f33b2bddaae863993f9c3a4ee38e54b5c2d1
Kyriakos Barbounakis <k.barbounakis@gmail.com>
2021-08-21 12:01:22 +0300

    add listener

---
+++ data-nested-queryable-listener.js
@@ -0,0 +1,57 @@
+// MOST Web Framework 2.0 Codename Blueshift BSD-3-Clause license Copyright (c) 2017-2021, THEMOST LP All rights reserved
+
+// eslint-disable-next-line no-unused-vars
+const { DataEventArgs } = require('./types');
+
+class DataNestedQueryableListener {
+    beforeExecute(event, callback) {
+        DataNestedQueryableListener.prototype.beforeExecuteAsync(event).then(function() {
+            return callback();
+        }).catch(function(err) {
+            return callback(err);
+        });
+    }
+    /**
+     * @param {DataEventArgs} event 
+     */
+    beforeExecuteAsync(event) {
+        const query = event.emitter && event.emitter.query;
+        const context = event.model.context;
+        if (query == null) {
+            return Promise.resolve();
+        }
+        if (Object.prototype.hasOwnProperty.call(query, '$expand')) {
+            /**
+             * @type {{ $entity:{ model:string }} | Array<{ $entity:{ model:string }}>}
+             */
+            const expand = query.$expand;
+            if (Array.isArray(expand)) {
+                const sources = expand.map(function(item) {
+                    if (item.$entity && item.$entity.model) {
+                        // get nested model
+                        const model = context.model(item.$entity.model);
+                        // if model exists and it's different from the current model
+                        if (model != null && event.model.name !== model.name) {
+                            // try to upgrade
+                            return model.migrateAsync();
+                        }
+                        return Promise.resolve();
+                    } 
+                });
+                return Promise.all(sources);
+            } else if (expand && expand.$entity && expand.$entity.model) {
+                // get nested model
+                const model = context.model(expand.$entity.model);
+                if (model != null && event.model.name !== model.name) {
+                    // try to upgrade
+                    return model.migrateAsync();
+                }
+            }
+        }
+        return Promise.resolve();
+    }
+}
+
+module.exports = {
+    DataNestedQueryableListener
+}
