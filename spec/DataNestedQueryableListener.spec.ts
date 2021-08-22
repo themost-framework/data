47c4044cdba50e7f4cc7d186455f607b2bb2da53
Kyriakos Barbounakis <k.barbounakis@gmail.com>
2021-08-22 15:47:45 +0300

    add empty app

---
+++ spec/DataNestedQueryableListener.spec.ts
@@ -0,0 +1,57 @@
+import { DataApplication } from '../data-application';
+import { resolve } from 'path';
+import { TraceUtils } from '@themost/common';
+import { DataConfigurationStrategy } from '../data-configuration';
+import { DataNestedQueryableListener } from '../data-nested-queryable-listener';
+
+describe('DataNestedQueryableListener', () => {
+    let app: DataApplication;
+    beforeAll((done) => {
+        app = new DataApplication(resolve(__dirname, 'empty'));
+        const configuration = app.getConfiguration();
+        configuration.setSourceAt('adapterTypes', [
+            {
+                'name':'Test Data Adapter', 
+                'invariantName': 'test',
+                'type': resolve(__dirname, 'adapter/TestAdapter')
+            }
+        ]);
+        configuration.setSourceAt('adapters', [
+            { 
+                'name': 'test-storage',
+                'invariantName': 'test',
+                'default':true,
+                "options": {
+                }
+            }
+        ]);
+        // reset data configuration
+        configuration.useStrategy(DataConfigurationStrategy, DataConfigurationStrategy);
+        return done();
+    });
+    it('should find listener', () => {
+        expect(app).toBeTruthy();
+        const context = app.createContext();
+        const model = context.model('Product');
+        expect(model).toBeTruthy();
+        let listeners = model.rawListeners('before.execute');
+        const beforeExecute = DataNestedQueryableListener.prototype.beforeExecute;
+        let listener = listeners.find((item) => {
+            return item === beforeExecute;
+        });
+        expect(listener).toBeTruthy();
+        model.removeListener('before.execute', beforeExecute);
+        listeners = model.rawListeners('before.execute');
+        listener = listeners.find((item) => {
+            return item === beforeExecute;
+        });
+        expect(listener).toBeFalsy();
+    });
+
+    it('should use nested query', async () => {
+        const context = app.createContext();
+        const items = await context.model('Action').where('actionStatus/alternateName').equal('CompletedActionStatus').silent().getItems();
+        expect(items).toBeTruthy();
+    });
+
+});
