faa04047a55b22bbf6acd8e283134ed6cc54a57d
Kyriakos Barbounakis <k.barbounakis@gmail.com>
2022-03-12 09:25:17 +0000

    add some extra tests

--- spec/DataExpandResolver.spec.ts
+++ spec/DataExpandResolver.spec.ts
@@ -25,13 +25,47 @@ fdescribe('DataExpandResolver', () => {
             }
         }]);
     });
+    it('should parse `orders($select=id,orderDate,orderedItem;$orderby=orderDate desc)`', () => {
+        const resolver = new DataExpandResolver();
+        expect(resolver.test('orders($select=id,orderDate,orderedItem;$orderby=orderDate desc)')).toEqual([{
+            name: 'orders',
+            options: {
+                $select: 'id,orderDate,orderedItem',
+                $orderby: 'orderDate desc'
+            }
+        }]);
+    });
+    it('should parse `orders($filter=year(orderDate) ge 2020;$orderby=orderDate desc)`', () => {
+        const resolver = new DataExpandResolver();
+        let res = resolver.test('orders($filter=year(orderDate) ge 2020;$orderby=orderDate desc)');
+        expect(res).toEqual([{
+            name: 'orders',
+            options: {
+                $filter: 'year(orderDate) ge 2020',
+                $orderby: 'orderDate desc'
+            }
+        }]);
+    });
     it('should parse `orders($expand=customer($expand=address))`', () => {
         const resolver = new DataExpandResolver();
-        const res = resolver.test('orders($expand=customer($expand=address))');
+        let res = resolver.test('orders($expand=customer($expand=address))');
         expect(res).toEqual([{
             name: 'orders',
             options: {
                 $expand: 'customer($expand=address)'
+            }
+        }]);
+        res = resolver.test('orders($expand=customer($expand=address)),tags($select=value;$orderby=value)');
+        expect(res).toEqual([{
+            name: 'orders',
+            options: {
+                $expand: 'customer($expand=address)'
+            }
+        }, {
+            name: 'tags',
+            options: {
+                $select: 'value',
+                $orderby: 'value'
             }
         }]);
     });
