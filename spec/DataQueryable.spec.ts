4a43bc61b515f9d7f5f6cda3f9f6d3a12c4699eb
Kyriakos Barbounakis <k.barbounakis@gmail.com>
2022-06-10 21:03:24 +0300

    test closures

--- spec/DataQueryable.spec.ts
+++ spec/DataQueryable.spec.ts
@@ -1,6 +1,7 @@
 import {resolve} from 'path';
 import {DataContext} from '../index';
 import {TestApplication} from './TestApplication';
+import {count} from "@themost/query";
 
 describe('DataQueryable', () => {
     let app: TestApplication;
@@ -23,10 +24,10 @@ describe('DataQueryable', () => {
 
     it('should use select closure', async () => {
         const items = await context.model('Product').asQueryable().select((product: any) => {
-                return {
-                    id: product.id,
-                    name: product.name
-                }
+            return {
+                id: product.id,
+                name: product.name
+            }
         }).getItems();
         expect(items).toBeInstanceOf(Array);
         for (const item of items) {
@@ -49,6 +50,78 @@ describe('DataQueryable', () => {
             }
         }).silent().getItems();
         expect(items).toBeInstanceOf(Array);
+    });
+
+    it('should use select closure with aggregate data', async () => {
+        const items = await context.model('Order').asQueryable().select((x: any) => {
+            return {
+                product: x.orderedItem.name,
+                total: count(x.id),
+            }
+        }).groupBy((x: any) => {
+            x.orderedItem.name
+        }).silent().getItems();
+        expect(items).toBeInstanceOf(Array);
+        for (const item of items) {
+            expect(item.total).toBeTruthy();
+            expect(item.total).toBeGreaterThan(0);
+        }
+    });
+
+    it('should use lower than', async () => {
+        let results = await context.model('Product').select((x: any) => {
+            x.id,
+            x.name,
+            x.price
+        }).where((x: any) => {
+            return x.price < 400;
+        }).silent().getItems();
+        expect(results.length).toBeTruthy();
+        results.forEach(x => {
+            expect(x.price).toBeLessThan(400);
+        });
+    });
+
+    it('should use greater than', async () => {
+        let results = await context.model('Product').select((x: any) => {
+            x.id,
+            x.name,
+            x.price
+        }).where((x: any) => {
+            return x.price > 400;
+        }).silent().getItems();
+        expect(results.length).toBeTruthy();
+        results.forEach(x => {
+            expect(x.price).toBeGreaterThan(400);
+        });
+    });
+
+    it('should use greater than or equal', async () => {
+        let results = await context.model('Product').select((x: any) => {
+            x.id,
+            x.name,
+            x.price
+        }).where((x: any) => {
+            return x.price >= 545.16;
+        }).silent().getItems();
+        expect(results.length).toBeTruthy();
+        results.forEach(x => {
+            expect(x.price).toBeGreaterThanOrEqual(545.16);
+        });
+    });
+
+    it('should use lower than or equal', async () => {
+        let results = await context.model('Product').select((x: any) => {
+            x.id,
+            x.name,
+            x.price
+        }).where((x: any) => {
+            return x.price <= 545.16;
+        }).silent().getItems();
+        expect(results.length).toBeTruthy();
+        results.forEach(x => {
+            expect(x.price).toBeLessThanOrEqual(545.16);
+        });
     });
 
 });
