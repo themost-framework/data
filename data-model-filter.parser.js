94194edf1aa0ff3f6050d7ced57662b01db766bf
Kyriakos Barbounakis <k.barbounakis@gmail.com>
2022-11-30 08:56:10 +0000

    create data filter parser

---
+++ data-model-filter.parser.js
@@ -0,0 +1,143 @@
+const { AsyncSeriesEventEmitter } = require('@themost/events');
+const { OpenDataParser } = require('@themost/query');
+const { DataAttributeResolver } = require('./data-attribute-resolver');
+const { DataFilterResolver } = require('./data-filter-resolver');
+
+class DataModelFilterParser {
+    /**
+     * 
+     * @param {import("./data-model").DataModel} model 
+     */
+    constructor(model) {
+        this.resolvingMember = new AsyncSeriesEventEmitter();
+        this.resolvingMethod = new AsyncSeriesEventEmitter();
+        this.model = model;
+    }
+    parse(str, callback) {
+        const self = this;
+        const thisModel = this.model;
+        const $expand = [];
+        const parser = new OpenDataParser();
+        parser.resolveMember = function(member, cb) {
+            const attr = thisModel.field(member);
+            if (attr) {
+                member = attr.name;
+                if (attr.multiplicity === 'ZeroOrOne') {
+                    var mapping1 = thisModel.inferMapping(member);
+                    if (mapping1 && mapping1.associationType === 'junction' && mapping1.parentModel === thisModel.name) {
+                        member = attr.name.concat('/', mapping1.childField);
+                    } else if (mapping1 && mapping1.associationType === 'junction' && mapping1.childModel === thisModel.name) {
+                        member = attr.name.concat('/', mapping1.parentField);
+                    }
+                }
+            }
+            const event = {
+                target: self,
+                member: member,
+                result: null
+            }
+            void self.resolvingMember.emit(event).then(() => {
+                if (event.result) {
+                    return cb(null, event.result.$select);
+                }
+                if (DataAttributeResolver.prototype.testNestedAttribute.call(thisModel,member)) {
+                    try {
+                        var member1 = member.split('/'),
+                            mapping = thisModel.inferMapping(member1[0]),
+                            expr;
+                        if (mapping && mapping.associationType === 'junction') {
+                            var expr1 = DataAttributeResolver.prototype.resolveJunctionAttributeJoin.call(thisModel, member);
+                            expr = {
+                                $expand: expr1.$expand
+                            };
+                            //replace member expression
+                            member = expr1.$select.$name.replace(/\./g,'/');
+                        }
+                        else {
+                            expr = DataAttributeResolver.prototype.resolveNestedAttributeJoin.call(thisModel, member);
+                            if (expr.$select) {
+                                member = expr.$select.$name.replace(/\./g,'/');
+                            }
+                        }
+                        if (expr && expr.$expand) {
+                            var arrExpr = [];
+                            if (Array.isArray(expr.$expand)) {
+                                arrExpr.push.apply(arrExpr, expr.$expand);
+                            } else {
+                                arrExpr.push(expr.$expand);
+                            }
+                            arrExpr.forEach(function(y) {
+                                var joinExpr = $expand.find(function(x) {
+                                    if (x.$entity && x.$entity.$as) {
+                                        return (x.$entity.$as === y.$entity.$as);
+                                    }
+                                    return false;
+                                });
+                                if (joinExpr == null)
+                                    $expand.push(y);
+                            });
+                        }
+                    }
+                    catch (err) {
+                        return cb(err);
+                    }
+                }
+                if (typeof thisModel.resolveMember === 'function') {
+                    thisModel.resolveMember.call(thisModel, member, cb);
+                } else {
+                    DataFilterResolver.prototype.resolveMember.call(thisModel, member, cb);
+                }
+            }).catch(function(err) {
+                return callback(err);
+            });
+            
+        }
+
+        parser.resolveMethod = function(name, args, cb) {
+            const thisModel = this.model;
+            const event = {
+                target: self,
+                method: name,
+                args: args,
+                result: null
+            }
+            void self.resolvingMethod.emit(event).then(function() {
+                if (event.result) {
+                    return cb(null, event.result);
+                }
+                if (typeof thisModel.resolveMethod === 'function') {
+                    thisModel.resolveMethod.call(thisModel, name, args, cb);
+                } else {
+                    DataFilterResolver.prototype.resolveMethod.call(thisModel, name, args, cb);
+                }
+            }).catch(function(err) {
+                return cb(err);
+            });
+        }
+
+        void parser.parse(str, function(err, result) {
+            if (err) {
+                return callback(err);
+            }
+            return callback(null, {
+                $where: result,
+                $expand: $expand
+            });
+        });
+    }
+    parseAsync(str) {
+        const self = this;
+        return new Promise(function(resolve, reject) {
+            void self.parse(str, function(err, result) {
+                if (err) {
+                    return reject(err);
+                }
+                return resolve(result);
+            });
+        });
+    }
+}
+
+module.exports = {
+    DataModelFilterParser
+}
---
+++ spec/DataPrivilege.exclude.spec.ts
@@ -0,0 +1,75 @@
+import {TestUtils} from './adapter/TestUtils';
+import { TestAdapter } from './adapter/TestAdapter';
+import { TestApplication2 } from './TestApplication';
+import { DataContext } from '../types';
+import {DataModelFilterParser} from '../data-model-filter.parser';
+import { at } from 'lodash';
+
+describe('DataPrivilege', () => {
+
+    let app: TestApplication2;
+    let context: DataContext;
+
+    beforeAll(async () => {
+        app = new TestApplication2();
+        context = app.createContext();
+    });
+
+    afterAll(async () => {
+        await context.finalizeAsync();
+        await app.finalize();
+    });
+
+    it('should validate exclude expression', async () => {
+        const Users = context.model('User');
+        Object.assign(context, {
+            user: {
+                name: 'luis.nash@example.com',
+                authenticationScope: 'profile'
+            }
+        })
+        const queryUsers = Users.asQueryable();
+        const parser = new DataModelFilterParser(Users);
+        const addSelect: any[] = [
+            {
+                "name": {
+                    $value: (context as any).user.name
+                }
+            }
+        ];
+        parser.resolvingMember.subscribe(async (event) => {
+            const propertyPath = event.member.split('/');
+            if (propertyPath[0] === 'context') {
+                propertyPath.splice(0, 1);
+                const property = at(context as any, propertyPath.join('.'))[0];
+                const propertyName = propertyPath[propertyPath.length - 1];
+                const exists = addSelect.findIndex((item) => Object.prototype.hasOwnProperty.call(item, propertyPath));
+                if (exists < 0) {
+                    const select = {};
+                    Object.defineProperty(select, propertyName , {
+                        enumerable: true,
+                        configurable: true,
+                        value: {
+                            $value: property
+                        }
+                    })
+                    addSelect.push(select);
+                }
+                event.result = {
+                    $select: "authenticationScope"
+                }
+            }
+        });
+        const q1 = await parser.parseAsync('context/user/authenticationScope eq \'profile\' or context/user/authenticationScope eq \'email\'');
+        expect(q1).toBeTruthy();
+        queryUsers.query.select([].concat(addSelect));
+        Object.assign(queryUsers.query, {
+            $where: q1.$where,
+            $expand: q1.$expand,
+            // $fixed: true
+        });
+        let result: any[] = await (context.db as TestAdapter).executeAsync(queryUsers.query);
+        expect(result).toBeInstanceOf(Array);
+        expect(result.length).toBeTruthy();
+    });
+});
