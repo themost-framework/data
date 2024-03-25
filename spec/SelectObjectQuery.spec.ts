d1f8f7436c7f2d48784d38c4f768073a3b99735c
Kyriakos Barbounakis <k.barbounakis@gmail.com>
2024-03-25 07:46:26 +0000

    validate results

--- spec/SelectObjectQuery.spec.ts
+++ spec/SelectObjectQuery.spec.ts
@@ -2,6 +2,8 @@ import { DataContext } from '../types';
 import { SelectObjectQuery } from '../select-object-query';
 import { TestApplication } from './TestApplication';
 import { resolve } from 'path';
+import { promisify } from 'util';
+import { DataQueryable } from '../data-queryable';
 
 
 describe('SelectParser', () => {
@@ -18,9 +20,27 @@ describe('SelectParser', () => {
         const Orders = context.model('Order').silent();
         const selectObject = new SelectObjectQuery(Orders);
         // get order
-        const item = await Orders.where('customer/email').equal('jesse.hawkins@example.com').getItem();
+        const item = await Orders.where('customer/email').equal('jesse.hawkins@example.com')
+            .and('orderStatus/name').equal('Processing')
+            .getItem();
         expect(item).toBeTruthy();
         const query = selectObject.select(item);
         expect(query).toBeTruthy();
-    })
+        expect(query.$fixed).toBeTruthy();
+        // parse filter
+        const filterAsync: (params: any) => Promise<DataQueryable> = promisify(Orders.filter).bind(Orders);
+        const q: DataQueryable = await filterAsync({
+            '$filter': 'orderStatus/name eq \'Processing\''
+        });
+        const { $prepared, $where, $expand } = q.query as any;
+        expect($expand).toBeTruthy();
+        Object.assign(query, {
+            $where,
+            $prepared,
+            $expand
+        });
+        const executeAsync: (query:any, values:Array<any>) => Promise<any[]> = promisify(context.db.execute).bind(context.db)
+        const results = await executeAsync(query, []);
+        expect(results).toBeTruthy();
+    });
 });
