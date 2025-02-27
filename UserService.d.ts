996a41f9c082cc25eeb0a38c1cbb97c1f585e999
Kyriakos Barbounakis <k.barbounakis@gmail.com>
2025-02-27 16:22:53 +0200

    implement user service

---
+++ UserService.d.ts
@@ -0,0 +1,24 @@
+import { ApplicationBase, ApplicationService } from '@themost/common';
+import { DataContext } from './types';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+export declare class UserService extends ApplicationService {
+    
+    constructor(app:any);
+
+    getUser(context: DataContext, name: string): Promise<any>;
+
+    getAnonymousUser(context: DataContext): Promise<any>;
+    
+    getGroup(context: DataContext, name: string): Promise<any>;
+
+    /**
+     * An observable that emits the anonymous user data.
+     * This can be used to track or handle the state of an anonymous user in the application.
+     * 
+     * @type {Observable<any>}
+     */
+    anonymousUser$: Observable<any>;
+
+    refreshAnonymousUser$: BehaviorSubject<any>;
+}
---
+++ UserService.js
@@ -0,0 +1,55 @@
+const { ApplicationService } = require('@themost/common');
+const { BehaviorSubject, shareReplay, switchMap, from, of } = require('rxjs');
+
+class UserService extends ApplicationService {
+  constructor(app) {
+    super(app);
+
+    this.refreshAnonymousUser$ = new BehaviorSubject(void 0);
+    
+    this.anonymousUser$ = this.refreshAnonymousUser$.pipe(switchMap((value) => {
+        if (value) {
+            return of(value);
+        }
+        // create a new context
+        const context = this.getApplication().createContext();
+        // get anonymous user
+        return from(this.getAnonymousUser(context).finally(() => {
+            // finalize context
+            return context.finalizeAsync();
+        }));
+    }), shareReplay(1));
+
+  }
+
+  /**
+   * @param {import('@themost/common').DataContextBase} context
+   * @param {string} name 
+   * @returns {Promise<any>}
+   */
+  getUser(context, name) {
+    return context.model('User').where('name').equal(name).expand('groups').silent().getItem();
+  }
+
+  /**
+   * @param {import('@themost/common').DataContextBase} context
+   * @param {string} name 
+   * @returns {Promise<any>}
+   */
+  getGroup(context, name) {
+    return context.model('Group').where('name').equal(name).silent().getItem();
+  }
+
+  /**
+   * @param {import('@themost/common').DataContextBase} context
+   * @returns {Promise<any>}
+   */
+  getAnonymousUser(context) {
+    return context.model('User').where('name').equal('anonymous').expand('groups').silent().getTypedItem();
+  }
+  
+}
+
+module.exports = {
+    UserService
+};
