785580fb49a51c4187501bf4d19815a4cb92f2d1
Kyriakos Barbounakis <k.barbounakis@gmail.com>
2025-03-08 08:46:50 +0200

    add local user service

---
+++ LocalUserService.d.ts
@@ -0,0 +1,5 @@
+import { UserService } from "./UserService";
+
+export declare class LocalUserService extends UserService {
+    constructor(app: any);
+}
---
+++ LocalUserService.js
@@ -0,0 +1,171 @@
+const { TraceUtils } = require('@themost/common');
+const { UserService } = require('./UserService');
+const { DataApplication } = require('./data-application');
+const { DataConfigurationStrategy } = require('./data-configuration');
+const { createInstance } = require('@themost/sqlite');
+const { DataContext } = require('./types');
+const { DataCacheStrategy } = require('./data-cache');
+const { performance } = require('perf_hooks');
+
+/**
+ * @type {import('@themost/common').DataModelProperties}
+ */
+const LocalUserServiceCache = {
+    name: 'LocalUserServiceCache',
+    version: '1.0.0',
+    source: 'LocalUserServiceCache',
+    view: 'LocalUserServiceCache',
+    fields: [
+        {
+            name: 'key',
+            type: 'Text',
+            primary: true,
+            nullable: false
+        },
+        {
+            name: 'value',
+            type: 'Json',
+            nullable: true
+        },
+        {
+            name: 'doomed',
+            type: 'Boolean',
+            nullable: true
+        },
+        {
+            name: 'metadata',
+            type: 'Json',
+            many: false,
+            nullable: true
+        },
+        {
+            name: 'createdAt',
+            type: 'DateTime',
+            nullable: true
+        },
+        {
+            name: 'expiresAt',
+            type: 'DateTime',
+            nullable: true
+        }
+    ]
+}
+
+class NoCacheStrategy extends DataCacheStrategy {
+
+    async get(key) {
+        return;
+    }
+
+    async add(key, value, absoluteExpiration) {
+        return;
+    }
+
+    async getOrDefault(key, getFunc, absoluteExpiration) {
+        return getFunc();
+    }
+
+    async remove(key) {
+        return;
+    }
+
+    async clear() {
+        return;
+    }
+
+    async finalize() {
+        return;
+    }
+}
+
+
+class LocalUserService extends UserService {
+
+
+    /**
+     * @param {import('@themost/common').ApplicationBase} app
+     */
+    constructor(app) {
+        super(app);
+
+        this.cacheApplication = new DataApplication(__dirname);
+        const configuration = this.cacheApplication.configuration.getStrategy(DataConfigurationStrategy);
+        Object.assign(configuration.adapterTypes, {
+            ['sqlite']: {
+                invariantName: 'sqlite',
+                name: 'sqlite',
+                createInstance: createInstance
+            }
+        });
+        configuration.adapters.push({
+            name: 'user+service+cache',
+            invariantName: 'sqlite',
+            default: true,
+            options: {
+                database: ':memory:'
+            }
+        });
+        configuration.setModelDefinition(LocalUserServiceCache);
+        const context = this.cacheApplication.createContext();
+        this.cache = context.model(LocalUserServiceCache.name);
+        // remove unused services
+    }
+
+    /**
+     * Get user by name
+     * @param {DataContext} context 
+     * @param {string} name 
+     * @returns 
+     */
+    async getUser(context, name) {
+        const start = performance.now();
+        let item = await this.cache.asQueryable().where('key').equal(name).getItem();
+        if (item) {
+            if (item.doomed) {
+                await this.cache.remove(item);
+            } else {
+                if (process.env.NODE_ENV === 'test' || process.env.NODE_ENV === 'development') {
+                    const end = performance.now();
+                    TraceUtils.log(`LocalUserService: Cache hit for user '${name}' in ${(end - start).toFixed(2)} ms.`);
+                }
+                return item.value;
+            }
+        }
+        const user = await super.getUser(context, name);
+        if (user) {
+            await this.cache.insert({
+                key: name,
+                value: user,
+                doomed: false,
+                createdAt: new Date()
+            });
+            return user;
+        }
+        return null;
+    }
+
+    /**
+     * Get anonymous user
+     * @param {DataContext} context 
+     * @returns 
+     */
+    getAnonymousUser(context) {
+        return super.getUser(context, 'anonymous');
+    }
+
+    /**
+     * Finalize user service
+     */
+    async finalizeAsync() {
+        await this.cache.context.finalizeAsync();
+        const service = this.cacheApplication.getConfiguration().getStrategy(DataCacheStrategy);
+        if (service != null && typeof service.finalize === 'function') {
+            await service.finalize();
+        }
+    }
+
+}
+
+module.exports = {
+    LocalUserService
+}
